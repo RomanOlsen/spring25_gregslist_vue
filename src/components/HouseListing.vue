bd533dac82b94fcc8639d8aa5cf41244f1118beb
RomanOlsen <romanjolsen@gmail.com>
2025-03-19 15:35:09 -0600

    draw houses

---
+++ src/components/HouseListing.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { House } from '@/models/House.js';
+
+
+defineProps({
+  houseProp: { type: House, required: true }
+})
+
+</script>
+
+
+<template>
+  <div class="card">
+    <div class="card-header"> {{ houseProp.year }} A.D. House | {{ houseProp.bedrooms }} Bed | {{ houseProp.bathrooms }}
+      Bath | {{ houseProp.levels }} Stories </div>
+    <div>Price: ${{ houseProp.price }}</div>
+    <span class="mdi mdi-house"></span>
+    <img :src="houseProp.imgUrl" alt="House picture">
+    <div></div>
+  </div>
+
+</template>
+
+
+<style lang="scss" scoped></style>
--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,19 +1,24 @@
 <script setup>
+import { AppState } from '@/AppState.js';
+import HouseListing from '@/components/HouseListing.vue';
 import { houseService } from '@/services/HouseService.js';
 import { Pop } from '@/utils/Pop.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 
+const houses = computed(() => AppState.houses)
 
 onMounted(() => {
   getHouses()
 })
 
-async function getHouses(){
+
+
+async function getHouses() {
   try {
     Pop.toast('getting houses')
     await houseService.getHouses()
   }
-  catch (error){
+  catch (error) {
     Pop.error(error);
   }
 }
@@ -22,10 +27,15 @@ async function getHouses(){
 
 
 <template>
-<div>homes</div>
+  <h1>HOUSES FOR SALE</h1>
+  <div class="container">
+    <div class="row">
+      <div v-for="house in houses" class="col-6">
+        <HouseListing :house-prop="house" />
+      </div>
+    </div>
+  </div>
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
